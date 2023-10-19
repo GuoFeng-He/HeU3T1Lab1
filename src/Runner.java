fbf5b25c0411405f8e0fa5557663e9cc2f9d75f7
BT_2N2_27 <sample@bths.edu>
2023-10-19 13:42:35 -0400

    initial commit (2)

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        if (x > 5) {
+            y = x;
+            if (x > 10) {
+                y--;
+            }
+        } else {
+            int z = x;
+            if (y >= z) {
+                z++;
+            } else {
+                z--;
+            }
+        }
+    }
 }
